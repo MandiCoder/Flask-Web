f46bd9020d8612e6bb31feaf875cb98d954b1780
MandiCoder <baitycasper@gmail.com>
2024-02-21 16:22:15 -0500

    but fix

--- modules/download_file.py
+++ modules/download_file.py
@@ -1,7 +1,8 @@
 
+import yt_dlp
+from unicodedata import normalize
 from requests import get
 from user_agent import generate_user_agent
-import yt_dlp
 from wget import download
 from os import mkdir
 from os.path import join, exists, basename
@@ -12,23 +13,25 @@ def download_file(url):
     path_download = 'downloads'
     if not exists('downloads'):
         mkdir('downloads')
-        
-    options = {
-                'format': 'best',
-                'outtmpl': join(path_download, '%(title)s.%(ext)s')    
-            }
     
     keywords = [
-        "youtu.be",
-        "short"
+       "youtu.be",
         "twitch",
         "fb.watch",
         "www.xvideos.com",
         "www.xnxx.com",
         "www.yourupload.com",
     ]
-    
-    if any(keyword in url for keyword in keywords): # -------------------------- DESCARGAR VIDEOS DE YOUTUBE
+
+
+    if any(keyword in url for keyword in keywords) or url.startswith("https://youtube.com/shorts"): # -------------------------- DESCARGAR VIDEOS DE YOUTUBE
+        title = get_title(url)
+        options = {
+                'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+                'outtmpl': join(path_download, f'{title}.%(ext)s')
+            }
+        
+        print("Titulo------------------------", title)
         with yt_dlp.YoutubeDL(options) as ydl: 
             ydl.download([url])
         return
@@ -57,3 +60,10 @@ def download_file(url):
         return
     
     
+    
+def get_title(url):
+    with yt_dlp.YoutubeDL() as ydl: 
+        info = ydl.extract_info(url, download=False)
+        title = info['title']
+        title = normalize("NFKD", title).encode("ascii", "ignore").decode("utf-8", "ignore")
+        return title.replace("#", "")
